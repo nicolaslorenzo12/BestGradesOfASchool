e3a1d14c50fadb7b1d9befa0c120c3a70fc3ce18
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-08-20 13:49:10 +0200

    Fixing flaws

--- src/grades/BestGradesPrinter.java
+++ src/grades/BestGradesPrinter.java
@@ -5,17 +5,14 @@ public class BestGradesPrinter {
     BestGradeCalculator bestGradeCalculator = new BestGradeCalculator();
     private Boy boy;
     private Girl girl;
-
-    public BestGradesPrinter(){
-        boy = bestGradeCalculator.getBestGradeBoys();
-        girl = bestGradeCalculator.getBestGradeGirls();
-    }
-
+    
     public void printingBoy(){
+        boy = bestGradeCalculator.getBestGradeBoys();
         System.out.println(boy.getName() + " " + boy.getGrade());
     }
 
     public void printingGirl(){
+        girl = bestGradeCalculator.getBestGradeGirls();
         System.out.println(girl.getName() + " " + girl.getGrade());
     }
 }
